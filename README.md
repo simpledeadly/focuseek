3d42adf3731153a8ce48ef05d17b1ca435179cf9
simpledeadly <markmarkov94186@gmail.com>
2024-08-07 23:59:24 +0700

    shallow's, triggerRef, toRaw, utilities, two lfcl hooks

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-# From easy to hard
-
 ### Have been studied or/and used:
 
-- ref()
-- reactive()
-- watch()
-- watchEffect()
-- computed()
+1. ref()
+2. reactive()
+3. watch()
+4. watchEffect()
+5. computed()
+6. shallowRef()
+7. shallowReactive()
+8. triggerRef()
+9. toRaw()
+10. isRef()
+11. isReactive()
+12. isProxy()
+13. onMounted()
+14. onUpdated()
 
 ### Misunderstandings
 
 ? watchPostEffect() | What means { flush: 'post' }?
 <br>
 ? watchSyncEffect() | What means { flush: 'sync' }?
+<br>
+? triggerRef() with shallowReactive()
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-import { computed, reactive, ref, watch, watchEffect } from 'vue'
+import {
+  isProxy,
+  isReactive,
+  isRef,
+  onMounted,
+  onUpdated,
+  reactive,
+  ref,
+  shallowReactive,
+  shallowRef,
+  toRaw,
+  triggerRef
+} from 'vue'
 
-type ObjType = { counter: number; someString: string }
+type ObjType = { counter: number; someString: string; location: { city: string } }
 
 const obj: ObjType = {
   counter: 0,
-  someString: 'one msg'
+  someString: 'one msg',
+  location: {
+    city: 'Novosibirsk'
+  }
 }
 
 const btn = ref(null)
@@ -15,27 +30,77 @@ const counter = ref<number>(0)
 
 const array = reactive<[string, boolean]>(['first', true])
 const counterInObject = reactive<ObjType>(obj)
+const counterInObject2 = shallowReactive<ObjType>(obj)
 
-watch(counter, (counter, prevCounter) => console.log('prev:', prevCounter, 'actual:', counter))
+const shallowObject = shallowReactive<ObjType>(obj)
 
-watchEffect(() => console.log(counter.value))
+// watch(shallowObject, () => console.log('Изменение обнаружено!')) // Отслеживает только верхний уровень
 
-let sum = ref<number>(0)
+const shalla = shallowRef<any>({ greet: 'JS!', nest: { someString: 'string' } })
 
-sum = computed<number>({
-  get: () => counter.value + counterInObject.counter,
-  set: (val) => (counter.value = val + 10)
-})
+// watchEffect(() => {
+//   console.log(shalla.value.nest.someString)
+// })
+
+// watchEffect(() => {
+//   console.log('from here:', shallowObject.location.city)
+// })
+
+// watch(shalla, (now, prev) => {
+//   console.log('was:', prev.greet, '| now:', now.greet)
+// })
+
+// watch(counter, (counter, prevCounter) => console.log('prev:', prevCounter, 'actual:', counter))
+
+// watchEffect(() => console.log(counter.value))
+
+// let sum = ref<number>(0)
+
+// sum = computed<number>({
+//   get: () => counter.value + counterInObject.counter,
+//   set: (val) => (counter.value = val + 10)
+// })
 // sum.value = 200
+
+function handleClick() {
+  shalla.value.nest.someString = 'number! :D'
+  triggerRef(shalla)
+}
+
+function handleClick2() {
+  // triggerRef(shallowObject)
+}
+
+console.log(isRef(counter))
+console.log(isRef(shalla))
+console.log(isReactive(shallowObject))
+console.log(isReactive(counterInObject2))
+console.log(toRaw(isProxy(counterInObject2)))
+console.log(isProxy(shalla))
+
+onMounted(() => {
+  console.log('mounted!', isRef(counter))
+})
+
+onUpdated(() => {
+  console.log('updated!', isRef(counter))
+})
 </script>
 
 <template>
-  <p>result computed: {{ sum }}</p>
+  <!-- <p>result computed: {{ sum }}</p> -->
   <button @click="counter++" ref="btn">Increment1 {{ counter }}</button>
   <button @click="counterInObject.counter++">Increment2 {{ counterInObject.counter }}</button>
   <br />
   <input type="checkbox" v-model="array[1]" />
   <input type="text" v-model="counterInObject.someString" />
+  <br />
+  <input type="text" v-model="shallowObject.someString" />
+  <input type="text" v-model="shallowObject.location.city" @change="console.log(shallowObject)" />
+  <br />
+  <input type="text" v-model="shalla.greet" @change="console.log(shalla.greet)" />
+  <div @click="handleClick">click to trigger ref</div>
+  <div @click="handleClick2">click to trigger reactive</div>
 </template>
 
 <style lang="scss">
