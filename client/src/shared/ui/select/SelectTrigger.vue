e3c1122ca0cb9c5de0e7bd4281c30c81f4d2beef
simpledeadly <markmarkov94186@gmail.com>
2025-04-14 11:19:39 +0700

    style(client): added style prop to component
    Added style prop to SelectTrigger.vue from shared.

--- client/src/shared/ui/select/SelectTrigger.vue
+++ client/src/shared/ui/select/SelectTrigger.vue
@@ -5,7 +5,7 @@ import { ChevronDown } from 'lucide-vue-next'
 import { cn } from '@/shared/lib/utils'
 
 const props = defineProps<
-  SelectTriggerProps & { class?: HTMLAttributes['class']; arrow?: boolean }
+  SelectTriggerProps & { class?: HTMLAttributes['class']; arrow?: boolean; badge?: boolean }
 >()
 
 const delegatedProps = computed(() => {
@@ -22,7 +22,8 @@ const forwardedProps = useForwardProps(delegatedProps)
     v-bind="forwardedProps"
     :class="
       cn(
-        'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 [&>span]:truncate text-start',
+        !props.badge &&
+          'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 [&>span]:truncate text-start',
         props.class
       )
     "
