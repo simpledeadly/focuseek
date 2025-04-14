99d44a32dc562b739cbe9290d42898ecf79b3d1e
simpledeadly <markmarkov94186@gmail.com>
2025-04-14 12:26:37 +0700

    refactor(client): updated add item inline form
    Changed mostly UI of add-item-form sticked under item-list widget.

--- client/src/features/item/add/index.ts
+++ client/src/features/item/add/index.ts
@@ -1,2 +1,3 @@
 export { useAddItem } from './model/useAddItem'
 export { default as AddItemForm } from './ui/AddItemForm.vue'
+export { default as AddItemFormInline } from './ui/AddItemFormInline.vue'
--- client/src/features/item/add/ui/AddItemFormInline.vue
+++ client/src/features/item/add/ui/AddItemFormInline.vue
@@ -1,86 +1,213 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Input } from '@/shared/ui/input'
-import { Button } from '@/shared/ui/button'
-import { Separator } from '@/shared/ui/separator'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/shared/ui/tooltip'
-import { useItems } from '@/entities/item'
-import { useFilterItems, useItemType } from '@/features/item/filter'
-import { Checkbox } from '@/shared/ui/checkbox'
-import { Popover } from '@/shared/ui/popover'
-import { PopoverContent, PopoverRoot, PopoverTrigger } from 'radix-vue'
+import { ref, watch } from 'vue'
 import { Badge } from '@/shared/ui/badge'
-import { Trash2 } from 'lucide-vue-next'
+import { Calendar } from '@/shared/ui/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+} from '@/shared/ui/select'
+import { cn } from '@/shared/lib/utils'
+import { useItemType } from '../../filter'
+import { DateFormatter, type DateValue, getLocalTimeZone } from '@internationalized/date'
+import { Checkbox } from '@/shared/ui/checkbox'
 
 const emit = defineEmits<{
-  (e: 'submit', data: { itemTitle: string; parentId?: number }): void
-}>()
-
-const slots = defineSlots<{
-  select?: () => unknown
+  (
+    e: 'submit',
+    data: {
+      itemTitle: string
+      parentId?: number
+      description?: string
+      deadline?: number
+      date?: number
+      priority?: number
+      durationPlanned?: number
+    }
+  ): void
 }>()
 
 const itemTitle = ref<string>('')
-const parentId = ref<number>()
+const itemDescription = ref<string>('')
+const itemDeadline = ref<number>()
+const itemDate = ref<number>()
+const itemParentId = ref<number>()
+const itemPriority = ref<number>()
+const itemDurationPlanned = ref<number>()
+
+const dateValue = ref<DateValue>()
+const deadlineValue = ref<DateValue>()
+
 const { itemType } = useItemType()
 
 const handleSubmit = () => {
   if (itemTitle.value.length > 0) {
-    const data = { itemTitle: itemTitle.value, parentId: Number(parentId.value) }
+    const data = {
+      itemTitle: itemTitle.value,
+      parentId: Number(itemParentId.value),
+      description: itemDescription.value,
+      deadline: Number(itemDeadline.value),
+      date: Number(itemDate.value),
+      priority: Number(itemPriority.value),
+      durationPlanned: Number(itemDurationPlanned.value),
+    }
     emit('submit', data)
   } else {
     alert('Введите заголовок')
   }
   itemTitle.value = ''
-  parentId.value = undefined
+  itemDescription.value = ''
+  itemDeadline.value = undefined
+  itemDate.value = undefined
+  itemPriority.value = undefined
+  itemDurationPlanned.value = undefined
+  dateValue.value = undefined
+  deadlineValue.value = undefined
 }
 
-const { items } = useItems()
-const { filteredItems } = useFilterItems(items)
+const df = new DateFormatter('en-US', {
+  dateStyle: 'long',
+})
+
+watch(dateValue, () => {
+  itemDate.value = +new Date(
+    dateValue.value?.year!,
+    dateValue.value?.month! - 1,
+    dateValue.value?.day
+  )
+})
+
+watch(deadlineValue, () => {
+  itemDeadline.value = +new Date(
+    deadlineValue.value?.year!,
+    deadlineValue.value?.month! - 1,
+    deadlineValue.value?.day
+  )
+})
 </script>
 
 <template>
-  <div class="add-item-wrapper-inline">
-    <div class="item-entity flex items-center space-x-4 rounded-md border pt-2 pb-2">
-      <div class="item-entity__row">
-        <div class="item-entity__row_part">
-          <div class="item-entity__checkbox">
-            <Checkbox class="item-checkbox" disabled />
-          </div>
-          <div class="item-entity__title">
-            <div class="item-title__inner-editable">
-              <input
-                type="text"
-                class="item-title__input"
-                placeholder="Enter title"
-              />
-            </div>
-          </div>
+  <div
+    class="add-item-wrapper-inline"
+    @keypress.enter="handleSubmit"
+  >
+    <div class="item-entity pt-2 pb-2">
+      <Checkbox
+        class="item-checkbox"
+        v-if="itemType !== 'note'"
+        :style="
+          (itemPriority === 1 && 'border: 2px solid red') ||
+          (itemPriority === 2 && 'border: 2px solid orange') ||
+          (itemPriority === 3 && 'border: 2px solid blue')
+        "
+        :disabled="true"
+      />
+      <div class="item-entity__column">
+        <div class="item-entity__title">
+          <input
+            v-model="itemTitle"
+            type="text"
+            placeholder="Enter title"
+            class="item-title__input"
+          />
         </div>
-        <div class="item-entity__row_part">
-          <div class="item-entity__timeLeft">
-            <PopoverRoot>
+        <div class="item-entity__description">
+          <input
+            v-model="itemDescription"
+            type="text"
+            placeholder="Enter description"
+            class="item-description__input"
+          />
+        </div>
+        <div class="item-entity__params">
+          <div class="item-entity__param">
+            <Popover>
               <PopoverTrigger as-child>
                 <Badge
                   variant="outline"
-                  class="item-deadline"
+                  :class="
+                    cn('justify-start text-left font-normal', !dateValue && 'text-muted-foreground')
+                  "
                 >
-                  {{ 'Add DL' }}
+                  {{ dateValue ? df.format(dateValue.toDate(getLocalTimeZone())) : 'Date' }}
                 </Badge>
               </PopoverTrigger>
-              <PopoverContent class="w-auto p-2 space-y-2">
-                <Input placeholder="YYYY-M-DD HH:mm" />
-                <Button>Save</Button>
+              <PopoverContent class="flex w-auto flex-col gap-y-2 p-2">
+                <Calendar v-model="dateValue" />
               </PopoverContent>
-            </PopoverRoot>
+            </Popover>
           </div>
-          <div class="item-entity__remove-button">
-            <button
-              type="button"
-              class="item-remove-button"
-            >
-              <Trash2 :size="16" />
-            </button>
+          <div class="item-entity__param">
+            <Popover>
+              <PopoverTrigger as-child>
+                <Badge
+                  variant="outline"
+                  :class="
+                    cn(
+                      'justify-start text-left font-normal',
+                      !deadlineValue && 'text-muted-foreground'
+                    )
+                  "
+                >
+                  {{
+                    deadlineValue ? df.format(deadlineValue.toDate(getLocalTimeZone())) : 'Deadline'
+                  }}
+                </Badge>
+              </PopoverTrigger>
+              <PopoverContent class="flex w-auto flex-col gap-y-2 p-2">
+                <Calendar v-model="deadlineValue" />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div class="item-entity__param">
+            <Select v-model="itemPriority">
+              <SelectTrigger badge>
+                <Badge
+                  variant="outline"
+                  :class="
+                    cn(
+                      'flex justify-start text-left font-normal',
+                      !itemPriority && 'text-muted-foreground'
+                    )
+                  "
+                >
+                  {{ itemPriority ? itemPriority : 'Set priority' }}
+                </Badge>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem :value="1">High</SelectItem>
+                  <SelectItem :value="2">Medium</SelectItem>
+                  <SelectItem :value="3">Low</SelectItem>
+                  <SelectItem :value="0">Clear</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div class="item-entity__param">
+            <Badge variant="outline">
+              <input
+                v-model="itemDurationPlanned"
+                type="number"
+                placeholder="Duration"
+                class="font-normal"
+                style="width: 4rem"
+              />
+            </Badge>
+          </div>
+          <div class="item-entity__param">
+            <Badge variant="outline">
+              <input
+                v-model="itemParentId"
+                type="number"
+                placeholder="pID"
+                class="font-normal"
+                style="width: 3rem"
+              />
+            </Badge>
           </div>
         </div>
       </div>
@@ -90,6 +217,19 @@ const { filteredItems } = useFilterItems(items)
 
 <style lang="scss">
 .add-item-wrapper-inline {
-  /** keep */
+  .item-entity {
+    border: none;
+
+    &__description input {
+      font-size: 15px;
+      color: hsl(var(--muted-foreground));
+    }
+
+    &__param {
+      display: flex;
+      max-width: 8rem;
+      cursor: pointer;
+    }
+  }
 }
 </style>
