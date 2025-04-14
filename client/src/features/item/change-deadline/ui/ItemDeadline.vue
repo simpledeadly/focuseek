5887cb03d9bc15ab05d8b35a0e536c28da8ec881
simpledeadly <markmarkov94186@gmail.com>
2025-04-14 12:13:46 +0700

    feat(client): updated item deadline feature
    Completely changed UI and logic of item deadline feature

--- client/src/features/item/change-deadline/ui/ItemDeadline.vue
+++ client/src/features/item/change-deadline/ui/ItemDeadline.vue
@@ -1,91 +1,48 @@
 <script setup lang="ts">
-import { Badge } from '@/shared/ui/badge'
-import { onMounted, onUnmounted, ref, watch } from 'vue'
-
+import { onMounted, ref, watch } from 'vue'
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover'
-import { Input } from '@/shared/ui/input'
+import { Badge } from '@/shared/ui/badge'
+import { cn } from '@/shared/lib/utils'
+import {
+  CalendarDate,
+  DateFormatter,
+  type DateValue,
+  getLocalTimeZone,
+} from '@internationalized/date'
+import { Calendar } from '@/shared/ui/calendar'
 import { Button } from '@/shared/ui/button'
 
-const props = defineProps<{
-  deadline?: string
-}>()
-
 const emit = defineEmits<{
-  (e: 'change', value: string): void
+  (e: 'change', value: number | undefined): void
 }>()
 
-const dateInput = ref<string>(props.deadline || '')
+const model = defineModel<number>()
+const deadlineValue = ref<DateValue>()
 
-const intervalId = ref<NodeJS.Timeout>()
-const targetDate = ref<Date>(new Date())
-
-const deadlineText = ref<string>('loading...')
-const timeLeft = ref<string>('Deadline will be here')
-
-// Обновляем targetDate при изменении dateInput
-watch(dateInput, (newValue) => {
-  try {
-    const parsedDate = new Date(newValue)
-    if (!isNaN(parsedDate.getTime())) {
-      targetDate.value = parsedDate
-    }
-  } catch (e) {
-    console.error('Error parsing date:', e)
-  }
+const df = new DateFormatter('ru-RU', {
+  dateStyle: 'long',
 })
 
 onMounted(() => {
-  if (props.deadline) {
-    try {
-      targetDate.value = new Date(props.deadline)
-    } catch (e) {
-      console.error('Error parsing initial deadline:', e)
-    }
+  if (model.value) {
+    const date = new Date(model.value)
+    deadlineValue.value = new CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate())
   }
-
-  intervalId.value = setInterval(() => {
-    const now = new Date()
-    const diff = targetDate.value.getTime() - now.getTime()
-
-    if (!props.deadline) {
-      deadlineText.value = 'Add DL'
-      return
-    }
-
-    const minutesAll = Math.floor(diff / (1000 * 60))
-
-    if (diff < 0) {
-      deadlineText.value = 'Expired 😵: ' + minutesAll * -1 + 'm'
-    } else {
-      deadlineText.value = `${minutesAll}m`
-
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-      const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000)
-      timeLeft.value = `Remained ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
-    }
-  }, 100)
 })
 
-onUnmounted(() => {
-  clearInterval(intervalId.value)
-})
-
-const handleSave = () => {
-  emit('change', dateInput.value)
-}
-
-const handleBadgeVariant = () => {
-  if (!props.deadline) {
-    return 'outline'
-  } else if (
-    parseInt(deadlineText.value.replace(/\D+/g, '')) <= 960 &&
-    !deadlineText.value.includes('Expired')
-  ) {
-    return 'destructive'
+watch(deadlineValue, (newVal) => {
+  if (newVal) {
+    model.value = new Date(newVal.year, newVal.month - 1, newVal.day).getTime()
   } else {
-    return 'secondary'
+    model.value = undefined
+  }
+})
+
+const handleSaveChanges = () => {
+  if (model.value) {
+    model.value = undefined
+    deadlineValue.value = undefined
+    emit('change', model.value)
   }
 }
 </script>
@@ -94,26 +51,34 @@ const handleBadgeVariant = () => {
   <Popover>
     <PopoverTrigger as-child>
       <Badge
-        :variant="handleBadgeVariant()"
-        class="item-deadline"
-        :title="timeLeft"
+        :variant="model ? 'secondary' : 'outline'"
+        :class="
+          cn('justify-start text-left font-normal', !deadlineValue && 'text-muted-foreground')
+        "
       >
-        {{ deadlineText }}
+        {{ deadlineValue ? df.format(deadlineValue.toDate(getLocalTimeZone())) : 'Deadline' }}
       </Badge>
     </PopoverTrigger>
-    <PopoverContent class="w-auto p-2 space-y-2">
-      <Input
-        v-model="dateInput"
-        placeholder="YYYY-M-DD HH:mm"
+    <PopoverContent class="flex w-auto flex-col gap-y-2 p-2">
+      <Calendar
+        v-model="deadlineValue"
+        @click="emit('change', model)"
       />
-      <Button @click="handleSave">Save</Button>
+      <Button
+        @click="handleSaveChanges"
+        :variant="model ? 'secondary' : 'outline'"
+      >
+        Clear
+      </Button>
     </PopoverContent>
   </Popover>
 </template>
 
 <style lang="scss">
-.item-deadline {
+.item-date {
   display: flex;
   cursor: pointer;
+  color: hsl(var(--muted-foreground));
+  font-weight: normal;
 }
 </style>
