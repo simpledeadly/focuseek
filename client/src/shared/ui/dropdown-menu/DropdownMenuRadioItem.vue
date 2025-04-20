a590a4448744a10c8922d167d9006c57a94ca71e
simpledeadly <markmarkov94186@gmail.com>
2025-04-20 17:24:36 +0700

    style(shared): changed radio item icon
    Changed icon from Circle to Check in dropdown-menu-radio-item component in shared folder.

--- client/src/shared/ui/dropdown-menu/DropdownMenuRadioItem.vue
+++ client/src/shared/ui/dropdown-menu/DropdownMenuRadioItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { cn } from '@/shared/lib/utils'
-import { Circle } from 'lucide-vue-next'
+import { Check } from 'lucide-vue-next'
 import {
   DropdownMenuItemIndicator,
   DropdownMenuRadioItem,
@@ -35,7 +35,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
   >
     <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <DropdownMenuItemIndicator>
-        <Circle class="h-4 w-4 fill-current" />
+        <Check class="h-4 w-4" />
       </DropdownMenuItemIndicator>
     </span>
     <slot />
