0dd63c5e99d1730a3151afd8b8901be0b487b5f8
simpledeadly <markmarkov94186@gmail.com>
2025-04-27 16:36:41 +0700

    feat(client): added detail page for item

---
+++ client/src/pages/item/ui/ItemPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useItems } from '@/entities/item'
+
+const { selectedItem } = useItems()
+console.log(selectedItem)
+</script>
+
+<template>
+  <div class="item-page">
+    <h1 v-if="selectedItem">{{ selectedItem.title }}</h1>
+    <h1 v-else>No Item Selected</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.item-page {
+  /** keep */
+}
+</style>
