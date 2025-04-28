948894ed713db1acb3992b7ac70a7f50ed9b79c0
simpledeadly <markmarkov94186@gmail.com>
2025-04-28 14:22:41 +0700

    refactor(client): combined 2 pages to 1

---
+++ client/src/pages/list/ui/ListPage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Collection, useCollections } from '@/entities/collection'
+import { ItemList } from '@/widgets/item-list'
+import { TabBar } from '@/widgets/tabbar'
+
+const emit = defineEmits<{
+  (e: 'collections', value: Collection[]): void
+  (e: 'user', value: Collection[]): void
+}>()
+
+const { collections } = useCollections()
+
+setTimeout(() => {
+  emit('collections', collections.value)
+}, 100)
+</script>
+
+<template>
+  <div class="list-page">
+    <TabBar />
+    <ItemList />
+  </div>
+</template>
+
+<style lang="scss">
+.list-page {
+  /** keep */
+}
+</style>
