d2fe0c94abe36057d73a3801047087fe1a89c65c
simpledeadly <markmarkov94186@gmail.com>
2025-04-28 14:43:29 +0700

    fix(client): fixed styles of 404 page

--- client/src/pages/not-found/ui/NotFound.vue
+++ client/src/pages/not-found/ui/NotFound.vue
@@ -6,21 +6,22 @@ import { Button } from '@/shared/ui/button'
 <template>
   <div class="not-found">
     <h1 style="font-size: 120px"><strong>404</strong></h1>
-    <h2 style="font-size: 30px; color: #aaa">Кажется вы... заблудились...</h2>
+    <h2 style="font-size: 30px; color: #777; margin-top: -20px">Кажется вы... заблудились...</h2>
     <br />
-    <br />
-    <Button @click="router.push({ name: 'collections' })"><strong>Но выход ЕСТЬ!</strong></Button>
+    <Button
+      variant="default"
+      @click="router.push({ name: 'collections' })"
+      ><strong>Но выход ЕСТЬ!</strong></Button
+    >
   </div>
 </template>
 
 <style lang="scss">
 .not-found {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 2;
+  margin-top: 30vh;
   display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
   width: 100%;
   height: 100%;
