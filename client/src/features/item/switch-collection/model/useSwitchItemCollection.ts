422c8cb61b5453ee7b2d6c083380eb273d622ba0
simpledeadly <markmarkov94186@gmail.com>
2025-05-03 12:58:41 +0700

    feat(client): added switching item collectionId for sub-items too

--- client/src/features/item/switch-collection/model/useSwitchItemCollection.ts
+++ client/src/features/item/switch-collection/model/useSwitchItemCollection.ts
@@ -1,12 +1,27 @@
 import { ShallowRef } from 'vue'
-import { updateItem, replaceItemInList, type Item } from '@/entities/item'
+import {
+  type Item,
+  updateItem,
+  replaceItemInList,
+  updateItemWithSubItems,
+  replaceItemsInList,
+} from '@/entities/item'
 import { updateItemOnServer } from '@/shared/api/api'
+import { useShowSubItems } from '../../show-sub-items'
 
 export const useSwitchItemCollection = (items: ShallowRef<Item[]>) => {
+  const { hasSubItems } = useShowSubItems(items)
+
   const switchItemCollection = async (item: Item, collectionId: number) => {
     const newItem = updateItem(item, { collectionId })
+
+    if (hasSubItems(item.id)) {
+      const newItems = updateItemWithSubItems(items.value, item, { collectionId })
+      items.value = replaceItemsInList(items.value, newItems)
+    } else {
+      items.value = replaceItemInList(items.value, newItem)
+    }
     await updateItemOnServer(item.id, newItem)
-    items.value = replaceItemInList(items.value, newItem)
   }
 
   return { switchItemCollection }
