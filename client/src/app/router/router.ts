73b24a46725450e4a2a9fe490640189ce0e46da3
simpledeadly <markmarkov94186@gmail.com>
2025-05-04 21:30:38 +0700

    feat(client): added dynamic title

--- client/src/app/router/router.ts
+++ client/src/app/router/router.ts
@@ -2,39 +2,44 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { isAuthenticated } from '../auth/auth'
 
 const routes = [
-  { path: '/register', component: () => import('@/pages/auth/register'), name: 'register' },
-  { path: '/login', component: () => import('@/pages/auth/login'), name: 'login' },
+  {
+    path: '/register',
+    component: () => import('@/pages/auth/register'),
+    name: 'register',
+    title: 'Регистрация',
+  },
+  { path: '/login', component: () => import('@/pages/auth/login'), name: 'login', title: 'Вход' },
   {
     path: '/collections',
     component: () => import('@/pages/collections'),
     name: 'collections',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, title: 'Коллекции' },
   },
   {
     path: '/:collection',
     component: () => import('@/pages/list'),
     name: 'list',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, title: 'Список' },
   },
   {
     path: '/:collection/:item',
     component: () => import('@/pages/item'),
     name: 'item',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, title: 'Элемент' },
   },
   {
     path: '/profile',
     component: () => import('@/pages/profile'),
     name: 'profile',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, title: 'Профиль' },
   },
   {
     path: '/settings',
     component: () => import('@/pages/settings'),
     name: 'settings',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, title: 'Настройки' },
   },
-  { path: '/404', component: () => import('@/pages/not-found'), name: '404' },
+  { path: '/404', component: () => import('@/pages/not-found'), name: '404', title: '404' },
   { path: '/:catchAll(.*)', redirect: { name: '404' } },
 ]
 
@@ -44,10 +49,12 @@ export const router = createRouter({
 })
 
 router.beforeEach((to, _from, next) => {
+  document.title = `${to.meta.title} | Focuseek` || 'Focuseek'
+  
   if (to.meta.requiresAuth && !isAuthenticated()) {
     next({ name: 'login' })
   } else if (to.params.collection === undefined && (to.name === 'todos' || to.name === 'notes')) {
-    next({ name: '404' }) // или перенаправьте на другой маршрут
+    next({ name: '404' })
   } else {
     next()
   }
