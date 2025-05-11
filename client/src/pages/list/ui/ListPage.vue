ab56d8f76f7d2527736fd56f6a63eaba7c3be7cc
simpledeadly <markmarkov94186@gmail.com>
2025-05-12 00:56:14 +0700

    refactor(client): removed unused/not actual logic

--- client/src/pages/list/ui/ListPage.vue
+++ client/src/pages/list/ui/ListPage.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import { Collection, useCollections } from '@/entities/collection'
 import { ItemListGroup } from '@/widgets/item-list'
 import { TabBar } from '@/widgets/tabbar'
-
-const emit = defineEmits<{
-  (e: 'collections', value: Collection[]): void
-  (e: 'user', value: Collection[]): void
-}>()
-
-const { collections } = useCollections()
-
-setTimeout(() => {
-  emit('collections', collections.value)
-}, 100)
 </script>
 
 <template>
