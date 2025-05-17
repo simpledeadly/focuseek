12d06edd46ac44478948fe623a885f010a61b23c
simpledeadly <markmarkov94186@gmail.com>
2025-05-17 22:09:38 +0700

    feat & fix(client): made absolutely working animations, fixed nesting

--- client/src/widgets/item-list/ui/ItemList.vue
+++ client/src/widgets/item-list/ui/ItemList.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { computed, nextTick, ref } from 'vue'
+import { Toggle } from '@/shared/ui/toggle'
 import { ItemEntity, Item } from '@/entities/item'
-import { AddItemFormInline } from '@/features/item/add'
+import { AddItemFormInline, AddSubItemFormInline } from '@/features/item/add'
 import { ItemTitle } from '@/features/item/change-title'
 import { ItemDescription } from '@/features/item/change-description'
 import { ItemDeadline } from '@/features/item/change-deadline'
@@ -11,21 +13,16 @@ import { ItemSubItemsToggle } from '@/features/item/show-sub-items'
 import { ItemOptions } from '@/features/item/options'
 import { ItemTimeTrack } from '@/features/item/time-track'
 import { useItemList } from '../composable/useItemList'
-import draggable from 'vuedraggable'
+import { ListTodo } from 'lucide-vue-next'
 
 const {
   filteredItems,
-  nestedItemsMap,
   itemType,
   collections,
   collectionId,
   showAllParams,
   isTimeTracking,
   updateItemProperty,
-  onDragStartNested,
-  onDragEndNested,
-  // onAddToNested,
-  // onRemoveFromNested,
   addItem,
   removeItem,
   toggleShowSubItems,
@@ -57,9 +54,81 @@ const emit = defineEmits<{
   (e: 'open-details-page', ...args: any[]): void
 }>()
 
-defineProps<{
+const props = defineProps<{
   item: Item
 }>()
+
+const container = ref<HTMLElement | null>(null)
+const isSubForm = ref<boolean>(false)
+const isExpandSubItemForm = ref<boolean>(false)
+const isExpandItemForm = ref<boolean>(false)
+
+const eal = ref<number>(-10)
+const duration = ref<number>(0.15)
+const timingFunction = ref<string>('cubic-bezier(0.55, 0, 0.1, 1)')
+
+const transitionString = computed<string>(() => {
+  return [
+    `max-height ${duration.value}s ${timingFunction.value}`,
+    `opacity ${duration.value}s ${timingFunction.value}`,
+    `transform ${duration.value}s ${timingFunction.value}`,
+  ].join(', ')
+})
+
+const transformString = (axis: 'X' | 'Y', value: number) =>
+  computed<string>(() => {
+    return `translate${axis}(${value}px)`
+  })
+
+function beforeEnter(el: Element) {
+  const element = el as HTMLElement
+  element.style.maxHeight = '0'
+  element.style.opacity = '0'
+  element.style.overflowY = 'hidden'
+  element.style.transform = transformString('Y', eal.value).value
+}
+
+function enter(el: Element, done: () => void) {
+  const element = el as HTMLElement
+  nextTick()
+  element.style.transition = transitionString.value
+  const height = element.scrollHeight
+  element.style.maxHeight = height + 'px'
+  element.style.opacity = '1'
+  element.style.transform = transformString('Y', 0).value
+  element.addEventListener(
+    'transitionend',
+    () => {
+      element.style.maxHeight = 'none'
+      element.style.overflowY = 'visible'
+      element.style.zIndex = '1'
+      done()
+    },
+    { once: true }
+  )
+}
+
+function leave(el: Element, done: () => void) {
+  const element = el as HTMLElement
+  element.style.maxHeight = element.scrollHeight + 'px'
+  element.style.opacity = '1'
+  element.style.transform = transformString('Y', 0).value
+  void element.offsetHeight
+  element.style.transition = transitionString.value
+  element.style.maxHeight = '0'
+  element.style.opacity = '0'
+  element.style.overflowY = 'hidden'
+  element.style.transform = transformString('Y', eal.value).value
+  element.addEventListener('transitionend', done, { once: true })
+}
+
+const handleToggle = (showSubs: boolean) => {
+  toggleShowSubItems(props.item)
+  if (!showSubs) {
+    isExpandSubItemForm.value = false
+    isExpandItemForm.value = false
+  }
+}
 </script>
 
 <template>
@@ -76,12 +145,12 @@ defineProps<{
     "
   >
     <template
-      v-if="hasSubItems(item.id) || item.showSubItems"
       #subItemsToggle
+      v-if="hasSubItems(item.id) || item.showSubItems"
     >
       <ItemSubItemsToggle
         :model-value="item.showSubItems"
-        @update:model-value="toggleShowSubItems(item)"
+        @update:model-value="handleToggle(item.showSubItems ?? false)"
       />
     </template>
     <template
@@ -151,6 +220,14 @@ defineProps<{
         @update:model-value="updateItemProperty(item, { priority: $event })"
       />
     </template>
+    <template
+      v-if="item.showSubItems"
+      #default
+    >
+      <Toggle v-model="isSubForm">
+        <ListTodo :class="isSubForm ? 'text-foreground' : 'text-muted-foreground'" />
+      </Toggle>
+    </template>
     <template #options>
       <ItemOptions
         :item="item"
@@ -195,67 +272,26 @@ defineProps<{
       />
     </template>
     <template #subItems>
-      <TransitionGroup name="sub-fade">
+      <Transition
+        name="sub-fade"
+        v-bind:css="false"
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @leave="leave"
+      >
         <div
-          v-if="item.showSubItems"
+          v-show="item.showSubItems"
           class="sub-items-container"
+          ref="container"
         >
-          <draggable
-            item-key="id"
-            v-model="nestedItemsMap[item.id]"
-            :group="{ name: 'items', pull: true, put: true }"
-            @start="(evt) => onDragStartNested(evt, item.id)"
-            @end="(evt) => onDragEndNested(evt, item.id)"
-            :component-data="{ name: 'fade-list', type: 'transition-group' }"
-            :animation="150"
-          >
-            <template #item="{ element }">
-              <div
-                :data-id="element.id"
-                :style="`--index: ${element.index};`"
-                class="sub-item"
-              >
-                <ItemList
-                  :item="element"
-                  :filtered-items="filteredItems"
-                  :collections="collections"
-                  :item-type="itemType"
-                  :show-all-params="showAllParams"
-                  :is-time-tracking="isTimeTracking"
-                  :has-sub-items="hasSubItems"
-                  :filter-nested-items="filterNestedItems"
-                  :nested-items="nestedItemsMap[element.id] || []"
-                  @toggle-done="(...args: any) => emit('toggle-done', ...args)"
-                  @change-title="(...args: any) => emit('change-title', ...args)"
-                  @change-description="(...args: any) => emit('change-description', ...args)"
-                  @change-duration-planned="
-                    (...args: any) => emit('change-duration-planned', ...args)
-                  "
-                  @change-duration-real="(...args: any) => emit('change-duration-real', ...args)"
-                  @delete-timer="(...args: any) => emit('delete-timer', ...args)"
-                  @change-date="(...args: any) => emit('change-date', ...args)"
-                  @change-deadline="(...args: any) => emit('change-deadline', ...args)"
-                  @change-priority="(...args: any) => emit('change-priority', ...args)"
-                  @switch-collection="(...args: any) => emit('switch-collection', ...args)"
-                  @switch-user="(...args: any) => emit('switch-user', ...args)"
-                  @switch-type="(...args: any) => emit('switch-type', ...args)"
-                  @remove="(...args: any) => emit('remove', ...args)"
-                  @toggle-show-sub-items="(...args: any) => emit('toggle-show-sub-items', ...args)"
-                  @add-description="(...args: any) => emit('add-description', ...args)"
-                  @remove-description="(...args: any) => emit('remove-description', ...args)"
-                  @open-details-page="(...args: any) => emit('open-details-page', ...args)"
-                />
-              </div>
-            </template>
-          </draggable>
-          <AddItemFormInline
+          <AddSubItemFormInline
             subForm
-            v-if="item.showSubItems"
             v-model:type="itemType"
-            key="add-item-form"
+            v-model:isExpand="isExpandSubItemForm"
+            key="add-sub-item-form"
             @submit="
               addItem(
-                collectionId,
+                collectionId ?? 0,
                 $event.itemTitle,
                 itemType,
                 item.id,
@@ -267,15 +303,74 @@ defineProps<{
               )
             "
           />
+          <TransitionGroup
+            name="fade-list"
+            mode="out-in"
+          >
+            <div
+              v-for="subItem in filterNestedItems(filteredItems, item.id)"
+              :key="subItem.id"
+              class="sub-item"
+            >
+              <ItemList
+                :item="subItem"
+                :filtered-items="filteredItems"
+                :collections="collections"
+                :item-type="itemType"
+                :show-all-params="showAllParams"
+                :is-time-tracking="isTimeTracking"
+                :has-sub-items="hasSubItems"
+                :filter-nested-items="filterNestedItems"
+                @toggle-done="(...args: any) => emit('toggle-done', ...args)"
+                @change-title="(...args: any) => emit('change-title', ...args)"
+                @change-description="(...args: any) => emit('change-description', ...args)"
+                @change-duration-planned="
+                  (...args: any) => emit('change-duration-planned', ...args)
+                "
+                @change-duration-real="(...args: any) => emit('change-duration-real', ...args)"
+                @delete-timer="(...args: any) => emit('delete-timer', ...args)"
+                @change-date="(...args: any) => emit('change-date', ...args)"
+                @change-deadline="(...args: any) => emit('change-deadline', ...args)"
+                @change-priority="(...args: any) => emit('change-priority', ...args)"
+                @switch-collection="(...args: any) => emit('switch-collection', ...args)"
+                @switch-user="(...args: any) => emit('switch-user', ...args)"
+                @switch-type="(...args: any) => emit('switch-type', ...args)"
+                @remove="(...args: any) => emit('remove', ...args)"
+                @toggle-show-sub-items="(...args: any) => emit('toggle-show-sub-items', ...args)"
+                @add-description="(...args: any) => emit('add-description', ...args)"
+                @remove-description="(...args: any) => emit('remove-description', ...args)"
+                @open-details-page="(...args: any) => emit('open-details-page', ...args)"
+              />
+            </div>
+            <AddItemFormInline
+              subForm
+              v-show="isSubForm"
+              v-model:type="itemType"
+              v-model:isExpand="isExpandItemForm"
+              key="add-sub-item-form"
+              @submit="
+                addItem(
+                  collectionId ?? 0,
+                  $event.itemTitle,
+                  itemType,
+                  item.id,
+                  $event.description,
+                  $event.deadline,
+                  $event.date,
+                  $event.priority,
+                  $event.durationPlanned
+                )
+              "
+            />
+          </TransitionGroup>
         </div>
-      </TransitionGroup>
+      </Transition>
     </template>
   </ItemEntity>
 </template>
-
+<!-- 
 <style lang="scss">
 .sub-items-container {
-  max-height: 3000px;
   transition:
     max-height 0.2s ease-in-out,
     opacity 0.2s ease-in-out;
@@ -296,9 +391,9 @@ defineProps<{
 
 .sub-item {
   transition:
+    max-height none,
     opacity 0.2s cubic-bezier(0.55, 0, 0.1, 1),
     transform 0.2s cubic-bezier(0.55, 0, 0.1, 1);
-  transition-delay: calc(0.01s * var(--index));
 }
 
 .sub-fade-enter-from .sub-item,
@@ -306,4 +401,4 @@ defineProps<{
   opacity: 0;
   transform: translateY(-4px);
 }
-</style>
+</style> -->
