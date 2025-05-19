91281de770dda914ac3e1686a82d5c2cadbfcf3a
simpledeadly <markmarkov94186@gmail.com>
2025-05-19 16:17:02 +0700

    feat(client): added pagination

--- client/src/widgets/item-list/ui/ItemListGroup.vue
+++ client/src/widgets/item-list/ui/ItemListGroup.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from '@/shared/ui/pagination'
+import { Button } from '@/shared/ui/button'
 import { useSidebar } from '@/shared/ui/sidebar'
 import { AddItemFormInline } from '@/features/item/add'
 import { Search } from '@/widgets/search'
@@ -24,10 +36,68 @@ const {
 } = useItemList()
 
 const { isMobile } = useSidebar()
+
+const itemsPerPage = 10
+const currentPage = ref(1)
+
+const paginatedItems = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage
+  const end = start + itemsPerPage
+  return sortedItems.value.slice(start, end)
+})
+
+const totalPages = computed(() => Math.ceil(sortedItems.value.length / itemsPerPage))
+
+const goToPage = (page: number) => {
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page
+  }
+}
 </script>
 
 <template>
   <Search />
+  <Pagination
+    :items-per-page="itemsPerPage"
+    :total="sortedItems.length"
+    :sibling-count="1"
+    show-edges
+    :default-page="currentPage"
+    @update:page="goToPage"
+  >
+    <PaginationList
+      v-slot="{ items }"
+      class="flex items-center gap-1"
+    >
+      <PaginationFirst @click="goToPage(1)" />
+      <PaginationPrev @click="goToPage(currentPage - 1)" />
+
+      <template v-for="(item, index) in items">
+        <PaginationListItem
+          v-if="item.type === 'page'"
+          :key="index"
+          :value="item.value"
+          as-child
+        >
+          <Button
+            class="w-8 h-8 p-0"
+            :variant="item.value === currentPage ? 'default' : 'outline'"
+            @click="goToPage(item.value)"
+          >
+            {{ item.value }}
+          </Button>
+        </PaginationListItem>
+        <PaginationEllipsis
+          v-else
+          :key="item.type"
+          :index="index"
+        />
+      </template>
+
+      <PaginationNext @click="goToPage(currentPage + 1)" />
+      <PaginationLast @click="goToPage(totalPages)" />
+    </PaginationList>
+  </Pagination>
   <div
     class="item-list"
     :style="isMobile && 'width: 85vw'"
@@ -39,7 +109,7 @@ const { isMobile } = useSidebar()
       <div :key="activeKey">
         <TransitionGroup name="fade-list">
           <div
-            v-for="item in sortedItems"
+            v-for="item in paginatedItems"
             :key="item.id"
           >
             <ItemList
